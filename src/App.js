9c8059c75ccf00049a134f06e2961ca20adb5c59
Tatia burdett <tatiaburdett@gmail.com>
2021-01-29 13:18:19 -0600

    build component tree

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Link } from 'react-router-dom'
+import Folders from './Folders/Folders'
+import Notes from './Notes/Notes'
+import data from './dummy-store'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.setState = {
+      data: data
+    }
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <header>
+          <Link to='/'><h1>Noteful</h1></Link>
+        </header>
+        <main>
+          <Notes />
+        </main>
+        <section>
+          <Folders />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Folders/Folders.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Route, Link } from 'react-router-dom'
+import './Folders.css'
+
+class Folders extends React.Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <Link to='/'><h1>Noteful</h1></Link>
+        </header>
+        <main>
+
+        </main>
+        <section>
+          
+        </section>
+      </div>
+    )
+  }
+}
+
+export default Folders
