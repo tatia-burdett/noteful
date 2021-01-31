9fd0265b0b67511e276623c79e6178cc68e011f0
Tatia burdett <tatiaburdett@gmail.com>
2021-01-31 11:47:56 -0600

    hover effects

--- src/FolderNote/FolderNote.js
+++ src/FolderNote/FolderNote.js
@@ -5,6 +5,11 @@ class FolderNote extends React.Component {
   render() {
     return ( 
       <div className='Folder_note'>
+        <div className='Folder_back_btn'>
+          <button onClick={() => this.props.history.goBack()}>
+            Go Back
+          </button>
+        </div>
         {this.props.folder && (
           <h3>{this.props.folder.name}</h3>
         )}
