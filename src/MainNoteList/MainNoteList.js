9ace138cfce1989b5070ffd93d6754260ed8d8fc
Tatia burdett <tatiaburdett@gmail.com>
2021-01-31 11:21:17 -0600

    format date

--- src/MainNoteList/MainNoteList.js
+++ src/MainNoteList/MainNoteList.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Moment from 'react-moment'
 import './MainNoteList.css'
 
 class MainNoteList extends React.Component {
+
   render() {
     return(
       <section className='Main_note_list'>
@@ -11,6 +13,7 @@ class MainNoteList extends React.Component {
             <li key={note.id} className='Main_list_item'>
               <Link to={`/note/${note.id}`}>
                 <h3>{note.name}</h3>
+                <Moment format='D MMM YYYY'>{note.modified}</Moment>
               </Link>
             </li>
           )}
