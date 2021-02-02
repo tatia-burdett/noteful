2ac9b54ce6c06b0dd994e34fc306e00613721025
Tatia burdett <tatiaburdett@gmail.com>
2021-02-02 10:15:54 -0600

    add context file with shape

--- src/FolderList/FolderList.js
+++ src/FolderList/FolderList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavLink, Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './FolderList.css'
 
 class FolderList extends React.Component {
