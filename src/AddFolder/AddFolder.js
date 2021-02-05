77d8ef3c414287f9ef0d6975e48dde0998dcfa75
Tatia burdett <tatiaburdett@gmail.com>
2021-02-05 15:05:59 -0500

    create working button

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,14 +1,26 @@
 import React from 'react'
 
 class AddFolder extends React.Component {
+
+  handleSubmit(event) {
+    event.preventDefault()
+    const name = event.target.name.value
+    console.log(name)
+  }
+
   render() {
     return (
       <div>
-        <form className='add-folder-form'>
+        <form className='add_folder_form' onSubmit={e => this.handleSubmit(e)}>
           <legend>Create a Folder</legend>
-          <label>Name</label>
-          <input></input>
-          <button>Add Folder</button>
+          <label htmlFor='name'>Name</label>
+          <input 
+            type='text'
+            name='name'
+            id='name'
+            className='add_folder'
+          />
+          <button type='submit'>Add Folder</button>
         </form>
       </div>
     )
