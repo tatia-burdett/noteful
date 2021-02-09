0bbafc5859c775626472206ec7ee2bbc2e4166b7
Tatia burdett <tatiaburdett@gmail.com>
2021-02-09 10:45:37 -0500

    fix typo

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -114,7 +114,7 @@ class AddNote extends React.Component {
     return (
       <div className='Add_note'>
         <form className='add_note_form' onSubmit={e => this.handleSubmit(e)}>
-          <legend>Create a Folder</legend>
+          <legend>Create a Note</legend>
           <label htmlFor='name' className='add_note_label'>Name:</label>
           <input 
             type='text'
